2c5c9bf9f7090db8265cf871a0192e49015d8902
akonst17 <akonst17@gmail.com>
2022-09-20 18:43:58 +0300

    интеграция sdk 2.0; новые урлы; build 13

--- DSM/Classes/Misc/GlobalDefinitions.swift
+++ DSM/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "ULCfasXZMG6D3V8H"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "1tH8jND1C9a6s5fK"
 
     static let applicationTag = "EXAM_DSM"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "1tH8jND1C9a6s5fK"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "uWmTRkbaqPldybvU"
 
     static let appStoreUrl = "https://apps.apple.com/app/dsm-5/id1576503453"
     static let appleAppID = "1576503453"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
